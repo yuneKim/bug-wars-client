159a30e995ea454689e53515338c3c19c4280fdb
Christopher Bandurski <cbandurski1@gmail.com>
2023-12-14 13:53:40 -0600

    test

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { RouterLink, RouterView } from 'vue-router';
+import HelloWorld from './components/HelloWorld.vue';
 </script>
 
 <template>
@@ -8,7 +8,7 @@ import HelloWorld from './components/HelloWorld.vue'
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it! Change Made" />
+      <HelloWorld msg="You did it! Change Made again" />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
