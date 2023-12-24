2a3d79a399a375b1240fb5dc1b77c0d0af6bda14
Christopher Bandurski <cbandurski1@gmail.com>
2023-12-24 07:50:45 -0600

    styled form

--- src/components/auth/LoginForm.vue
+++ src/components/auth/LoginForm.vue
@@ -6,13 +6,26 @@ const loginDto = ref<LoginDto>({
   username: '',
   password: '',
 });
+
+function submit() {
+  console.log(loginDto.value);
+}
 </script>
 
 <template>
   <div class="login-wrapper">
-    <form @submit.prevent="">
-      <label for="username">Username</label>
-      <input type="text" id="username" />
+    <form class="login-form" @submit.prevent="submit">
+      <div class="form-group">
+        <label for="username">Username</label>
+        <input type="text" id="username" v-model="loginDto.username" />
+      </div>
+      <div class="form-group">
+        <label for="password">Password</label>
+        <input type="password" id="password" v-model="loginDto.password" />
+      </div>
+      <div class="form-group">
+        <button type="submit">Login</button>
+      </div>
     </form>
   </div>
 </template>
@@ -20,6 +33,17 @@ const loginDto = ref<LoginDto>({
 <style scoped>
 .login-wrapper {
   margin: 0 auto;
-  max-width: 1000px;
+  max-width: 250px;
+}
+
+.login-form {
+  display: flex;
+  flex-direction: column;
+  gap: 15px;
+}
+
+.form-group {
+  display: flex;
+  flex-direction: column;
 }
 </style>
