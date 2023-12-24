0c18366c9079d54fc00b839b601b2cca8a743167
Christopher Bandurski <cbandurski1@gmail.com>
2023-12-24 07:50:50 -0600

    created authservice

---
+++ src/services/AuthService.ts
@@ -0,0 +1,8 @@
+import type { LoginDto as loginDto } from '@/types';
+import axios from 'axios';
+
+export default {
+  login(loginDto: loginDto) {
+    return axios.post('/auth/login', loginDto);
+  },
+};
