d778fba36ee633fddee4a052167c12dec667e123
Stephanie <stranosteph@gmail.com>
2024-01-06 16:48:15 -0500

    Changed router link case

--- src/components/game/LandingPage.vue
+++ src/components/game/LandingPage.vue
@@ -4,10 +4,10 @@
 <template>
     <div>
         <!-- logo goes here -->
-        <button><router-link v-bind:to="{name: 'gameLobby'}" data-test="gameLobby">Play Game</router-link></button>
-        <button><router-link v-bind:to="{name: 'scriptEditor'}" data-test="scriptEditor">Script Editor</router-link></button>
-        <button><router-link v-bind:to="{name: 'howToPlay'}" data-test="howToPlay">How to Play</router-link></button>
-        <button><router-link v-bind:to="{name: 'credits'}" data-test="credits">Credits</router-link></button>
+        <button><RouterLink v-bind:to="{name: 'gameLobby'}" data-test="gameLobby">Play Game</RouterLink></button>
+        <button><RouterLink v-bind:to="{name: 'scriptEditor'}" data-test="scriptEditor">Script Editor</RouterLink></button>
+        <button><RouterLink v-bind:to="{name: 'howToPlay'}" data-test="howToPlay">How to Play</RouterLink></button>
+        <button><RouterLink v-bind:to="{name: 'credits'}" data-test="credits">Credits</RouterLink></button>
     </div>
 </template>
 
