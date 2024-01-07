f93acc2eb0dbb74139da334eec63f5ce40151c6c
Samantha Stinchcomb <samanthalstinchcomb@gmail.com>
2024-01-07 18:13:38 -0500

    created post for scriptService and test coverage

--- src/services/scriptService.ts
+++ src/services/scriptService.ts
@@ -1,4 +1,4 @@
-import type { ParseDto } from '@/types';
+import type { ParseDto, ScriptDto } from '@/types';
 import { makeRequest } from '@/utils/makeRequest';
 import axios from 'axios';
 
@@ -26,6 +26,15 @@ export const scriptService = {
         401: 'You must be logged in to view scripts.',
         403: 'You do not have permission to view this script.',
         404: 'Script not found.',
+      },
+    });
+  },
+  createScript(scriptDto: ScriptDto) {
+    return makeRequest(() => axios.post('/auth/login', scriptDto), {
+      successStatuses: [200],
+      errorStatuses: {
+        400: 'Name/Script cannot be blank.',
+        409: 'Name must be unique.',
       },
     });
   },
