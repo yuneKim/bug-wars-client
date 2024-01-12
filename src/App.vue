716e65382da6357137501ec646260f4ab07eabf0
Stephanie <stranosteph@gmail.com>
2024-01-11 20:55:28 -0500

    Styled home page and nav bars

--- src/App.vue
+++ src/App.vue
@@ -8,24 +8,67 @@ const { logout } = useAuthStore();
 </script>
 
 <template>
-  <header>
-    <div class="wrapper">
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <a class="logout" href="" v-if="user.username" @click.prevent="logout(false)">Logout</a>
-        <RouterLink class="login" v-else :to="{ name: 'login' }">Login</RouterLink>
-        <RouterLink :to="{ name: 'register' }">Register</RouterLink>
-        <RouterLink :to="{ name: 'scriptEditor' }">Script Editor</RouterLink>
+  <div class="container">
+    <header>
+      <nav class="header-div">
+        <div class="left">
+          <RouterLink :to="{ name: 'gameLobby' }">Play Game</RouterLink>
+          <RouterLink :to="{ name: 'scriptEditor' }">Script Editor</RouterLink>
+        </div>
+        <div class="middle"><h1 class="main-title">Bug Wars</h1></div>
+        <div class="right">
+          <RouterLink to="/">Home</RouterLink>
+          <a class="logout" href="" v-if="user.username" @click.prevent="logout(false)">Logout</a>
+          <RouterLink class="login" v-else :to="{ name: 'login' }">Login</RouterLink>
+          <RouterLink :to="{ name: 'register' }">Register</RouterLink>
+        </div>
       </nav>
-    </div>
-  </header>
+    </header>
 
-  <RouterView />
+    <RouterView />
+    <footer>
+      <nav class="footer-nav">
+        <RouterLink :to="{ name: 'howToPlay' }">How to Play</RouterLink>
+        <RouterLink :to="{ name: 'credits' }">Credits</RouterLink>
+      </nav>
+    </footer>
+  </div>
 </template>
 
 <style scoped>
-header {
-  max-height: 100vh;
+.container {
+  display: grid;
+  grid-template-rows: auto 1fr auto;
+  position: absolute;
+  inset: 0;
+}
+
+.header-div {
+  display: grid;
+  grid-template-columns: 1fr auto 1fr;
+}
+
+.left {
+  justify-self: end;
+  display: flex;
+  align-items: center;
+  margin-right: 75px;
+}
+
+.center {
+  display: flex;
+  align-items: center;
+}
+
+.right {
+  justify-self: start;
+  display: flex;
+  align-items: center;
+  margin-left: 75px;
+}
+
+.main-title {
+  margin: 0;
 }
 
 nav {
@@ -37,5 +80,11 @@ nav {
 
 nav a {
   padding: 0 1rem;
+}
+
+.footer-nav {
+  display: flex;
+  justify-content: flex-end;
+  padding-bottom: 5px;
 }
 </style>
--- src/components/game/LandingPage.vue
+++ src/components/game/LandingPage.vue
@@ -1,14 +1,50 @@
 <script setup lang="ts">
+import Button from 'primevue/button';
 </script>
 
 <template>
-    <div>
-        <!-- logo goes here -->
-        <button><RouterLink :to="{name: 'gameLobby'}" data-test="gameLobby">Play Game</RouterLink></button>
-        <button><RouterLink :to="{name: 'scripts'}" data-test="scriptEditor">Script Editor</RouterLink></button>
-        <button><RouterLink :to="{name: 'howToPlay'}" data-test="howToPlay">How to Play</RouterLink></button>
-        <button><RouterLink :to="{name: 'credits'}" data-test="credits">Credits</RouterLink></button>
+  <div class="container">
+    <div class="btn-wrapper">
+      <!-- logo goes here -->
+      <Button>
+        <RouterLink :to="{ name: 'gameLobby' }" data-test="gameLobby">Play Game</RouterLink>
+      </Button>
+      <Button>
+        <RouterLink :to="{ name: 'scripts' }" data-test="scriptEditor">Script Editor</RouterLink>
+      </Button>
+      <Button>
+        <RouterLink :to="{ name: 'howToPlay' }" data-test="howToPlay">How to Play</RouterLink>
+      </Button>
+      <Button>
+        <RouterLink :to="{ name: 'credits' }" data-test="credits">Credits</RouterLink>
+      </Button>
     </div>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.container {
+  display: flex;
+  justify-content: center;
+  padding-top: calc(100vh / 4);
+}
+
+.btn-wrapper {
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+}
+
+.p-button {
+  justify-content: center;
+  background: red;
+  border-radius: 0;
+  border-color: red;
+  padding-block: 5px;
+}
+
+.btn-wrapper a {
+  text-decoration: none;
+  color: white;
+}
+</style>
