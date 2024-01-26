4e18e3ef6917e6827dd84026333684520ab45332
Stephanie <stranosteph@gmail.com>
2024-01-25 19:13:00 -0500

    Mobile styling

--- src/components/base/TopNav.vue
+++ src/components/base/TopNav.vue
@@ -48,12 +48,6 @@ const hideNavLinks = () => {
         <RouterLink @click="hideNavLinks" class="login" v-else :to="{ name: 'login' }"
           >Login</RouterLink
         >
-        <RouterLink v-if="user.username" @click="hideNavLinks" :to="{ name: 'profile' }">Profile</RouterLink>
-        <!--Added Profile button if user is logged in-->
-        <RouterLink v-if="user.username" :to="{ name: 'profilesettings' }" class="profile-settings-link">
-        <i class="pi pi-cog" style="font-size: 1.5rem;"></i>
-        </RouterLink>
-        <!--Added profile settings button if user is logged. Button is a cogwheel-->
       </div>
 
       <span class="nav-btn">
