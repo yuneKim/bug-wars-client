92d12af6a7a5badf846d4fcfc23d9ba1964a6af4
Samantha Stinchcomb <samanthalstinchcomb@gmail.com>
2024-02-01 11:08:23 -0500

    added blurbs

--- src/components/demo/TheDemo.vue
+++ src/components/demo/TheDemo.vue
@@ -1,7 +1,9 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Divider from 'primevue/divider';
+</script>
 
 <template>
-  <div class="how-to-play-container">
+  <div class="demo-container">
     <div class="card-wrapper">
       <div class="card">
         <h1>DEMO VIDEO</h1>
@@ -20,7 +22,6 @@
       <div class="card">
         <h2>OUR PROCESS</h2>
         <div class="content"></div>
-        <Divider />
         <h3>SERVER-SIDE TECHNOLOGIES AND TOOLS</h3>
         <div class="content">
           <p>
@@ -43,11 +44,30 @@
           </p>
         </div>
         <Divider />
-        <h3>LABEL</h3>
+        <h3>DEVELOPMENT OPERATIONS</h3>
         <div class="content">
           <p>
-            A <strong>LABEL</strong> serves as a target for a <strong>CONTROL STATEMENT</strong>.
-            Define a LABEL like:
+            To ensure that our application will run regardless of the particular environment our
+            developers are using, Bug Wars is containerized using Docker. Bug Wars was created in a
+            CI/CD pipeline. CI/CD stands for continuous integration & continuous development. This
+            means exactly what it sounds like! We work in small subsections of code called stories,
+            that are broken in even smaller parts called tasks. Once a developer or team of
+            developers completes a story in their local branch, they then make a pull request. Their
+            pull request is both checked by another member of our team and also triggers automatic
+            builds to check that test coverage is complete and the new code is compatible with the
+            existing codebase. After those steps, it’s moved to delivery and deployed to production.
+          </p>
+        </div>
+        <Divider />
+        <h3>SOFTWARE DEVELOPMENT LIFE CYCLE</h3>
+        <div class="content">
+          <p>
+            In order to build Bug Wars, our team is taking an iterative approach by using the Agile
+            framework. Our stories are written in two-week sprints, delivering a demo and receiving
+            a review at the end of a sprint cycle. Directly after that, we have a retrospective to
+            reflect on both what went well and what could be improved, and we plan out our next
+            sprint. This Agile methodology allows us to be adaptable and adjust our plans as needed
+            throughout the project.
           </p>
         </div>
       </div>
@@ -56,7 +76,7 @@
 </template>
 
 <style scoped>
-.how-to-play-container {
+.demo-container {
   height: 100%;
   display: flex;
   flex-direction: column;
@@ -102,10 +122,6 @@
   line-height: 1.7rem;
 }
 
-.card strong {
-  font-size: 1.4rem;
-}
-
 .content {
   margin-top: 50px;
   display: flex;
@@ -118,11 +134,6 @@
     flex-direction: column-reverse;
   }
 
-  .content img {
-    max-width: 250px;
-    margin-block: 0;
-  }
-
   .content p {
     margin-block: 0;
   }
@@ -130,55 +141,6 @@
 
 .content > p {
   flex: 5;
-}
-
-.content img {
-  flex: 3;
-  min-width: 0;
-}
-
-.battleground-content > p {
-  flex: 1;
-}
-
-.battleground-content img {
-  flex: 1;
-}
-
-.content .code {
-  background: #282828;
-  white-space: pre-wrap;
-  font-family: var(--editor-font-family);
-  font-size: 1rem;
-  line-height: var(--editor-line-height);
-  padding: 10px;
-}
-
-.table {
-  margin-top: 30px;
-}
-
-.table table {
-  border-collapse: collapse;
-}
-
-.table th,
-td {
-  font-size: 1.2rem;
-  line-height: 1.7rem;
-  padding: 6px;
-}
-
-.table th:first-child,
-td:first-child {
-  border-right: 1px solid #535353;
-}
-
-.table th {
-  text-align: left;
-  font-size: 1.5rem;
-  font-weight: 500;
-  border-bottom: 1px solid #535353;
 }
 
 .p-divider {
