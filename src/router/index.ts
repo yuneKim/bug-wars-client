b4fd6e3300c6318eb5a330b4e46a4a112adce539
kido8013 <keaton.kido@gmail.com>
2024-02-03 22:30:28 -0800

    Change to main removing all files after branch merge removed all files after pulling. Re-added all components/views/auth/routing back.

--- src/router/index.ts
+++ src/router/index.ts
@@ -64,6 +64,18 @@ const router = createRouter({
       component: () => import('@/views/DemoView.vue'),
       meta: { requiresAuth: false },
     },
+    {
+      path: '/profile',
+      name: 'profile',
+      component: () => import ('@/views/ProfileView.vue'),
+      meta: { requiresAuth: true },
+    },
+    {
+      path: '/profilesettings',
+      name: 'profilesettings',
+      component: () => import ('@/views/ProfileSettingsView.vue'),
+      meta: { requiresAuth: true },
+    },
   ],
 });
 
---
+++ src/views/ProfileSettingsView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import ProfileSettings from '@/components/profile/ProfileSettings.vue';
+</script>
+
+<template>
+  <ProfileSettings />
+</template>
+
+<style scoped></style>
